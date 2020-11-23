8bd9dc5bc025aee4ad462273018521278153fc1f
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-23 16:16:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
 
-def load_library
+require 'pry'
+require "yaml"
+def load_library(path)
+  emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
+  original = YAML.load_file(path)
+  binding.pry
+  original.each do |meaning, describe| 
+    emoticons[meaning] = describe
+ #   binding.pry
+      end
+    end
+ # binding.pry
+  emoticons
+end
+
+# hash = Hash[array.collect{|symbol| ["", symbol]}]
+#return ex 
+#emoticons = { emotion1 => {:english => translation, :japanese => translation}, emotion2 {}}
+
+
+
+def get_japanese_emoticon(path, emoticon)
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
+def get_english_meaning(path, emoticon)
+  YAML.load_file(path)
   # code goes here
 end
